2d1c47b4d0d46c3e37e8b7870d8603b7e4205fc7
zhaozhongjie <power_zzj@163.com>
2016-03-03 17:50:46 +0800

    由赵仲捷编写的CG项目，开源工具包，准备中......

---
+++ ServerClient/test_ui.py
@@ -0,0 +1,9 @@
+__author__ = 'zhaozhongjie'
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import os,sys
+
+app = QApplication(sys.argv)
+xxx = QPushButton("aaa")
+xxx.show()
+sys.exit(app.exec_())
